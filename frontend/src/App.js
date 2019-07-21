a23cdef9526ac95326db373600bf79ed0535d0b4
Ignacio <shoik.xtazy@gmail.com>
2019-07-21 17:09:53 -0400

    routes in navbar ok and key for each li :D

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 import Navigation from './components/Navigation';
@@ -11,7 +12,10 @@ function App() {
   return (
   <Router>
     <Navigation/>
-    <Route path="/" component={NotesList}/>
+    <Route exact path="/" component={NotesList}/>
+    <Route path="/edit/:id" component={CreateNote}/>
+    <Route path="/create" component={CreateNote}/>
+    <Route path="/user" component={CreateUser}/>
   </Router>
   );
 }
--- frontend/src/components/CreateUser.js
+++ frontend/src/components/CreateUser.js
@@ -1,10 +1,47 @@
 import React, { Component } from 'react'
+import axios from 'axios';
 
 export default class CreateUser extends Component {
+
+    state = {
+        users: [],
+        username: ''
+    }
+
+    async componentDidMount(){
+        const res = await axios.get('http://localhost:4000/api/users');
+        this.setState({users:res.data});
+        console.log(this.state.users);
+    }
+
+    onChangeUserName = (e) => {
+        console.log(e.target.value);
+    }
+
     render() {
         return (
-            <div>
-                Create User
+            <div className="row">
+                <div className="col-md-4">
+                    <h3>Create New User</h3>
+                    <form>
+                        <div className="form-group">
+                            <input 
+                                type="text" 
+                                className="form-control" 
+                                onChange={this.onChangeUserName}/>
+                        </div>
+                    </form>
+                </div>
+                <div className="col-md-8">
+                    <ul className="list-group">
+                        {
+                            this.state.users.map(user => (
+                                <li className="list-group-item list-group-action" key={user._id}>
+                                    {user.username}
+                                </li>))
+                        }
+                    </ul>
+                </div>
             </div>
         )
     }
